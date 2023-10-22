60b4c587ec504bcaf5bbc8b372a07a389da35682
Robert Odhiambo <odhizrober254@gmail.com>
2023-10-22 18:46:41 +0000

    files: sort.h, bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - SOrts an array in ascending order using Bubble Sort algorithm
+  * @array: Array being sorted
+  * @size: Size of the array
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int check;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		check = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				check = 1;
+			}
+		}
+
+		if (check == 0)
+			break;
+	}
+}
+
+/**
+  * swap - Swaps an element with another
+  * @first: Reference Element
+  * @last: ELement swapped with first
+  */
+void swap(int *first, int *last)
+{
+	int temp = *first;
+
+	*first = *last;
+	*last = temp;
+}
